2ca6a9f8af83e919da146dd3e61848052b41a113
palm90150 <palm90150@gmail.com>
2024-11-26 11:26:11 +0700

    ้hhh

---
+++ Hello world/Hello world/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Wichaphop Sodsai";
+// string is Datatype คือชนิดของข้อมูล
+// name is variable
+//"..." is value of variable
+Console.WriteLine("Hello, World!");
+Console.WriteLine(name);
+Console.WriteLine("My name is Wichaphop Sodsai");
+Console.WriteLine("My student ID 673450394-8");
+
